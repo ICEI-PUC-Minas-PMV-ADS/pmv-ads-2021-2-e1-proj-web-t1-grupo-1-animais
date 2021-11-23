cb0c2c71d129f1bb44805ec120017de34eb31b8d
ciro <cw.artiga@gmail.com>
2021-11-22 23:31:04 -0300

    atualização filtro

--- src/menu.js
+++ src/menu.js
@@ -3,3 +3,11 @@ function abrirMenu(){
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+function filtro(){
+    var itemBusca = document.querySelectorAll('li.itemBusca');
+    for (var i = 0; i < itemBusca.length; i++){
+        itemBusca[i].classList.toggle("abrirFiltro");
+    }
+    document.getElementById("itensBusca").classList.toggle("abrirFiltro");
+}
