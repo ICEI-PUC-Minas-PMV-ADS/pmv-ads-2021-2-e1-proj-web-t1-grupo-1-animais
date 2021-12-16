83445a8a200ef24be970afbc6777113d85bcec70
ciro <cw.artiga@gmail.com>
2021-12-16 10:20:11 -0300

    opção nao encontrado (filtro pesquisa)

--- src/especies/especies.js
+++ src/especies/especies.js
@@ -1,10 +1,10 @@
-function abrirMenu(){
+function abrirMenu(){       // Abre o menu hamburguer
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
-function filtro(){
+function filtro(){      // Abre e fecha o filtro de pesquisa quando clica
     var itemBusca = document.querySelectorAll('li.itemBusca');
     for (var i = 0; i < itemBusca.length; i++){
         itemBusca[i].classList.toggle("abrirFiltro");
@@ -12,16 +12,33 @@ function filtro(){
     document.getElementById("itensBusca").classList.toggle("abrirFiltro");
 }
 
+function filtro2(){     // Abre o filtro de pesquisa forçosamente (usado na função teclando())
+    var itemBusca = document.querySelectorAll('li.itemBusca');
+    for (var i = 0; i < itemBusca.length; i++){
+        itemBusca[i].classList.toggle("abrirFiltro", false);
+    }
+    document.getElementById("itensBusca").classList.toggle("abrirFiltro", false);
+}
+
 function teclando(){
+    filtro2();      // Força o site a abrir a lista de espécies
     var texto = document.getElementById("txtBusca");
     var filtro = texto.value.toUpperCase();
     var lista = document.getElementById("itensBusca");
     var itens = lista.getElementsByTagName("li");
+    var notFound = document.getElementById("notFound");
+    var counter = 0;
     for (var i = 0; i < itens.length; i++){
         if (itens[i].innerText.toUpperCase().indexOf(filtro) == 0){
             itens[i].style.display = "";
+            counter++
         } else{
             itens[i].style.display = "none";
         }
+    }
+    if (counter == 0){      // Se o input digitado pelo usuário não corresponder a nenhum item da lista de espécies, o item notFound será exibido
+        notFound.style.display = "block";
+        notFound.style.color = "grey";
+        notFound.style.cursor = "default";
     }
 }
